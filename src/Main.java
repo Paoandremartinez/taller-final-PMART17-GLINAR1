31545f639acd81d697391e61777058ea3af735a1
Gersain1 <gersain.linares@hotmail.com>
2024-08-16 16:24:51 -0500

    Se hace menu de datos primitivos

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("GLINA1");
+        System.out.println("******************************************************");
+        System.out.println("****************** DATOS PRIMITIVOS ******************\n");
+        System.out.println("01. Explicación byte.");
+        System.out.println("02. Explicación short.");
+        System.out.println("03. Explicación int");
+        System.out.println("04. Explicación long");
+        System.out.println("05. Explicación float");
+        System.out.println("06. Explicación double");
+        System.out.println("07. Explicación char");
+        System.out.println("08. Explicación bolean");
+        System.out.println("09. Salir\n");
+        System.out.println("******************************************************");
+
     }
 }
