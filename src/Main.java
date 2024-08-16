9bf6090e16ba0c468f0877d4b506973e23d3355e
paoandremartinez <paoandremartinez@gmail.com>
2024-08-16 16:25:26 -0500

    se realiza creación menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        
 
-        System.out.println("Hello world!");
-        System.out.println("GLINA1");
+        System.out.println("******************************************************");
+        System.out.println("*******        TALLER PRE-HACKATON          **********");
+        System.out.println("*******     GERSAIN LINARES - GLINAR1       **********");
+        System.out.println("*******      PAOLA MARTINEZ - PMART17       **********");
+        System.out.println("******************************************************");
+        System.out.println("******************************************************");
+        System.out.println("**                                                  **");
+        System.out.println("**    1. Datos primitivos                           **");
+        System.out.println("**    2. String                                     **");
+        System.out.println("**    3. Constantes                                 **");
+        System.out.println("**    4. Tipos de Operadores                        **");
+        System.out.println("**    5. Condicional IF, ELSE IF, ELSE              **");
+        System.out.println("**    6. Condicional Switch                         **");
+        System.out.println("**    7. Condicional Ternaria                       **");
+        System.out.println("**    8. Bucle DO WHILE                             **");
+        System.out.println("**    9. Bucle WHILE                                **");
+        System.out.println("**   10. Bucle FOR                                  **");
+        System.out.println("**   00. Salir                                      **");
+        System.out.println("******************************************************");
+        System.out.println("Por favor digita tu mejor opción:  ");
+
     }
 }
