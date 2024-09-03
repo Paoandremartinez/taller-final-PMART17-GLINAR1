bece64123b4419771b7fd38ac6c50b0c83530bd5
paoandremartinez <paoandremartinez@gmail.com>
2024-09-03 17:49:26 -0500

    arreglo menu ternario

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scannerMainFirst = new Scanner(System.in);
 
-        int opcion = 0;
-        int opc4 = 0;
+        int opcion = 0; // variable utilizada en el menu principal
+        int opc1 = 0; // variable utilizada en el menu de datos primitivos
+        int opc4 = 0; // variable utilizada en el menu de tipo operadores
+        int opc7 = 0; // variable utilizada en el menu de Ternaria
+        int opc72= 0; // variable utilizada en el menu de Ternaria - programa
+        int num =0; // variable usada en el programa de la ternaria
         do {
             System.out.println("******************************************************");
             System.out.println("*******        TALLER PRE-HACKATON          **********");
@@ -45,24 +49,26 @@ public class Main {
             switch (opcion)
             {
                 case 1:
-                    System.out.println("******************************************************************");
-                    System.out.println("*******              1. DATOS PRIMITIVOS                   *******");
-                    System.out.println("******************************************************************");
-                    System.out.println("******************************************************************");
-                    System.out.println("*     1. Explicación byte.                                       *");
-                    System.out.println("*     2. Explicación short.                                      *");
-                    System.out.println("*     3. Explicación int                                         *");
-                    System.out.println("*     4. Explicación long                                        *");
-                    System.out.println("*     5. Explicación float                                       *");
-                    System.out.println("*     6. Explicación double                                      *");
-                    System.out.println("*     7. Explicación char                                        *");
-                    System.out.println("*     8. Explicación bolean                                      *");
-                    System.out.println("*     9. Salir                                                   *");
-                    System.out.println("*                                                                *");
-                    System.out.println("******************************************************************");
-                    System.out.println("Por favor digita tu mejor opción:  ");
-                    break;
-                case 2:
+
+                        System.out.println("******************************************************************");
+                        System.out.println("*******              1. DATOS PRIMITIVOS                   *******");
+                        System.out.println("******************************************************************");
+                        System.out.println("******************************************************************");
+                        System.out.println("*     1. Explicación byte.                                       *");
+                        System.out.println("*     2. Explicación short.                                      *");
+                        System.out.println("*     3. Explicación int                                         *");
+                        System.out.println("*     4. Explicación long                                        *");
+                        System.out.println("*     5. Explicación float                                       *");
+                        System.out.println("*     6. Explicación double                                      *");
+                        System.out.println("*     7. Explicación char                                        *");
+                        System.out.println("*     8. Explicación bolean                                      *");
+                        System.out.println("*     9. Salir                                                   *");
+                        System.out.println("*                                                                *");
+                        System.out.println("******************************************************************");
+                        System.out.println("Por favor digita tu mejor opción:  ");
+
+                        break;
+               case 2:
                     System.out.println("******************************************************************");
                     System.out.println("*******                  2. STRING                      **********");
                     System.out.println("******************************************************************");
@@ -91,10 +97,17 @@ public class Main {
                     System.out.println("*  3. Logicos                                                    *");
                     System.out.println("*  4. Asignación                                                 *");
                     System.out.println("*  5. Incremento / Decremento                                    *");
-                    System.out.println("*  6. Volver al menú unicial                                     *");
+                    System.out.println("*  6. Volver al menú inicial                                     *");
                     System.out.println("*                                                                *");
                     System.out.println("******************************************************************");
-                    System.out.println("Por favor digita tu mejor opción:  ");
+                    System.out.println("Por favor digita tu opción:  ");
+                    /*while (!scannerMainFirst.hasNextInt());
+                          {
+                              System.out.println("Por favor digita una opción valida");
+                              scannerMainFirst.next();
+                              System.out.println("Ingresa la opción");
+                          }*/
+
                     opc4 = scannerMainFirst.nextInt();
 
 
@@ -244,27 +257,15 @@ public class Main {
                                     scannerMainFirst.nextLine();
 
                                 }
-
-
-
                           }
-                          /*while (!scannerMainFirst.hasNextInt());
-                          {
-                              System.out.println("Por favor digita una opción valida");
-                              scannerMainFirst.next();
-                              System.out.println("Ingresa la opción");
-                          }*/
-                       //   opc4 = scannerMainFirst.nextInt();
-
-
-
-                        }while (opc4 != 6);
+                       }while (opc4 != 6);
                    break;
                 case 5:
                     break;
                 case 6:
                     break;
-                case 7:/*
+                case 7:
+                do {
                     System.out.println("******************************************************************");
                     System.out.println("*******             7. TERNARIA                            *******");
                     System.out.println("******************************************************************");
@@ -276,34 +277,70 @@ public class Main {
                     System.out.println("*                                                                *");
                     System.out.println("******************************************************************");
                     System.out.println("Por favor digita tu mejor opción:  ");
-                    int opc7 = scannerMainFirst.nextInt();
-                    do{
+                    opc7 = scannerMainFirst.nextInt();
+
                     switch (opc7)
                     {
-                        case 1;
-                        break;
+                        case 1:
+                            System.out.println("******************************************************************");
+                            System.out.println("*******         1. EXPLICACIÓN TERNARIA                 **********");
+                            System.out.println("******************************************************************");
+                            System.out.println("* El operador ternario en Java es una construcción que permite   *");
+                            System.out.println("* tomar decisiones basadas en una condición booleana de manera   *");
+                            System.out.println("* concisa en una sola línea de código                            *");
+                            System.out.println("*                                                                *");
+                            System.out.println("*     String respuesta = (num % 2 == 0) ? \"Par\" : \"Impar\";   *");
+                            System.out.println("******************************************************************");
+                            break;
+                        case 2:
+                            System.out.println("******************************************************************");
+                            System.out.println("*******           2. PROGRAMA TERNARIA                  **********");
+                            System.out.println("******************************************************************");
+                            System.out.println("*                                                                *");
+                            System.out.println("* ¿Quieres saber si tu número es par o impar?                    *");
+                            System.out.println("*  1. Si                                                         *");
+                            System.out.println("*  2. No                                                         *");
+                            System.out.println("*  Ingresa la opción :                                           *");
+                            System.out.println("******************************************************************");
+                            opc72 = scannerMainFirst.nextInt();
 
-                    }
-                    }
-                    while*/
+                            switch (opc72)
+                            {
+                                case 1:
+                                    System.out.println("Ingresa el número ");
+                                    num = scannerMainFirst.nextInt();
+                                    String respuesta = (num % 2 == 0) ? "Par" : "Impar";
+                                    System.out.println("El número es " + respuesta);
+                                    break;
+                                case 2:
+                                    System.out.println("Vuelve después..... ");
+                                    break;
+                                default:
+                                    System.out.println("Número no válido, por favor verifique");
+                            }
 
-                    break;
+                            break;
+                            default:
+
+                                if
+                                (opc72 == 3) {
+                                    System.out.println("Si quieres volver al menú Principal presiona enter");
+                                    scannerMainFirst.nextLine();
+                                    scannerMainFirst.nextLine();
+
+                                }
+                    }
+                    }while (opc7 != 3);
+                  break;
                 case 8:
                     break;
                 case 9:
                     break;
                 case 0:
                     break;
-            }
+            } //break;
 
-
-
-
-
- /*
-
-*/
-        }
+         }
         while (opcion!=3);
         scannerMainFirst.close();
     }
