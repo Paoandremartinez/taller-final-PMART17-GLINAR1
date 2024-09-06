cc833d43b6d1b3548018b4821f333bdb4a806495
Gersain1 <gersain.linares@hotmail.com>
2024-09-06 16:03:10 -0500

    actiualización2

--- src/Main.java
+++ src/Main.java
@@ -83,20 +83,68 @@ public class Main {
                         break;
                         case 2:
                             System.out.println("*******************************************************************");
-                            System.out.println("*******               1. Explicación short                **********");
+                            System.out.println("*******               1. Explicación short                *********");
                             System.out.println("*******************************************************************");
-                            System.out.println("*    *");
-                            System.out.println("*     *");
-                            System.out.println("*     *");
+                            System.out.println("*short es uno de los tipos de datos primitivos que se utiliza para*");
+                            System.out.println("*representar números enteros. Es una alternativa a int para cuando*");
+                            System.out.println("* necesitas almacenar valores enteros en un rango más pequeño.    *");
                             System.out.println("*******************************************************************");
                         break;
                         case 3:
                             System.out.println("*******************************************************************");
                             System.out.println("*******               1. Explicación int                **********");
                             System.out.println("*******************************************************************");
-                            System.out.println("*    *");
-                            System.out.println("*     *");
-                            System.out.println("*     *");
+                            System.out.println("*int es un tipo de dato primitivo que se utiliza para representar *");
+                            System.out.println("*números enteros. Es uno de los ocho tipos de datos primitivos    *");
+                            System.out.println("*en Java y es conocido por su eficiencia y rapidez en la          *");
+                            System.out.println("manipulación de datos.                                            *");
+                            System.out.println("*******************************************************************");
+                            break;
+                        case 4:
+                            System.out.println("*******************************************************************");
+                            System.out.println("*******               1. Explicación long                **********");
+                            System.out.println("*******************************************************************");
+                            System.out.println("*long es un tipo de dato primitivo utilizado para representar     *");
+                            System.out.println("*números enteros más grandes que los que puede manejar el tipo    *");
+                            System.out.println("*int. Es útil cuando necesitas trabajar con valores enteros que   *");
+                            System.out.println(" exceden el rango de un int.                                      *");
+                            System.out.println("*******************************************************************");
+                            break;
+                        case 5:
+                            System.out.println("********************************************************************");
+                            System.out.println("*******               1. Explicación float                **********");
+                            System.out.println("********************************************************************");
+                            System.out.println("*float es un tipo de dato primitivo que se utiliza para            *");
+                            System.out.println("*representar números en punto flotante de precisión simple.        *");
+                            System.out.println("********************************************************************");
+                            break;
+                        case 6:
+                            System.out.println("********************************************************************");
+                            System.out.println("*******               1. Explicación double                *********");
+                            System.out.println("********************************************************************");
+                            System.out.println("*double es un tipo de dato primitivo utilizado para representar    *");
+                            System.out.println("*números en punto flotante de precisión doble. Esto significa que  *");
+                            System.out.println("* puede almacenar números con decimales y ofrece mayor precisión   *");
+                            System.out.println("que el tipo float.                                                 *");
+                            System.out.println("********************************************************************");
+                            break;
+                        case 7:
+                            System.out.println("*******************************************************************");
+                            System.out.println("*******               1. Explicación char                **********");
+                            System.out.println("*******************************************************************");
+                            System.out.println("*char es un tipo de dato primitivo que se utiliza para representar*");
+                            System.out.println("* un solo carácter. Los caracteres en Java están representados    *");
+                            System.out.println("*mediante el código Unicode, lo que permite que el tipo char      *");
+                            System.out.println(" maneje caracteres de una amplia gama de idiomas y símbolos.      *");
+                            System.out.println("*******************************************************************");
+                            break;
+                        case 8:
+                            System.out.println("********************************************************************");
+                            System.out.println("*******               1. Explicación bolean                *********");
+                            System.out.println("********************************************************************");
+                            System.out.println("*boolean es un tipo de dato primitivo que se utiliza para          *");
+                            System.out.println("*representar valores lógicos, es decir, valores que pueden ser     *");
+                            System.out.println("*solo uno de dos posibles: true (verdadero) o false (falso).       *");
                             System.out.println("*******************************************************************");
                             break;
                         default:
